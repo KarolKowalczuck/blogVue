3d0e175205b3d0545780e1d92c21fa03cdd86874
Karolina <karolinaborgesk@gmail.com>
2023-05-16 09:22:56 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          title: "Meu primeiro post",
+          datetime: Date.now(),
+          content: "Postar aqui é muito legal",
+        },
+        {
+          title: "Meu segundo post",
+          datetime: Date.now(),
+          content: "Postar aqui não é muito legal",
+        },
+      ],
+      formData: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  methods: {
+    handleClick(event) {
+      const now = new Date();
+
+      const dataDaPostagem = `${now.getDate()}/${
+        now.getMonth() + 1
+      }/${now.getFullYear()}`;
+
+      this.posts.push({
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      });
+
+      this.formData = {
+        title: "",
+        content: "",
+      };
+    },
+
+    handleInputChange(event) {
+      const { name, value } = event.target;
+      this.formData[name] = value;
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="lista-posts">
+    <div class="post" v-for="post in posts" :key="post.key">
+      <h3>{{ post.title }}</h3>
+      <h4>{{ post.datetime }}</h4>
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+
+  <form action="">
+    <input v-model="formData.title" placeholder="Titulo" />
+    <textarea
+      name="content"
+      :value="formData.content"
+      placeholder="Escreva seu post aqui.."
+      @keyup="handleInputChange"
+      id=""
+      cols="30"
+      rows="10"
+    >
+    </textarea>
+
+    <button type="button" @click="handleClick">Salvar</button>
+  </form>
+
+  <RouterView />
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+form > * {
+  margin: 1rem;
+}
+</style>
