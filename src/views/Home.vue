f6f1a132ac2b8e1dd8ec202cc524e4ea3a03d3a6
Karolina <karolinaborgesk@gmail.com>
2023-05-17 15:10:46 -0300

    implementa listagem de posts na home

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: {
+    posts: Array,
+  },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  computed: {
+    filteredPosts() {
+      // se search estiver vazio, retorne a lista completa de posts
+      if (!this.search) return this.posts;
+
+      // se tiver qualquer coisa em search, faz o filtro
+      const listaFiltrada = [];
+      for (const post of this.posts) {
+        if (post.title.includes(this.search)) {
+          listaFiltrada.push(post);
+        }
+      }
+      return listaFiltrada;
+    },
+  },
+};
+</script>
+
+<template>
+  <input
+    v-model="search"
+    placeholder="Procure pelo título do post..."
+    class="search"
+  />
+
+  <div id="lista-posts" class="flex">
+    <div class="post flex" v-for="post in filteredPosts" :key="post.key">
+      <h3>{{ post.title }}</h3>
+      <h4>{{ post.datetime }}</h4>
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+</template>
